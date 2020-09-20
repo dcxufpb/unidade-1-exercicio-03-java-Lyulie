e330f7393c737ddeede93056a415202bef2f126f
Rodolpho Alves <rrpa15@live.com>
2020-09-20 12:06:28 -0300

    main

--- src/main/java/com/example/project/CupomFiscal.java
+++ src/main/java/com/example/project/CupomFiscal.java
@@ -15,8 +15,65 @@ public class CupomFiscal {
 	public static String CNPJ = "42.591.651/0797-34";
 	public static String INSCRICAO_ESTADUAL = "244.898.500.113";
 
+	public static void imprimir_dados(){
+		if(NOME_LOJA.isEmpty()) {
+			System.out.printf("O campo nome da loja é obrigatório");
+			return;
+		}
+
+		if (LOGRADOURO.isEmpty()) {
+			System.out.printf("O campo logradouro do endereço é obrigatório");
+			return;
+		}
+
+		String _logradouro = LOGRADOURO + ", ";
+		String _numero = NUMERO <= 0? "s/n" : String.format("%d", NUMERO);
+		String _complemento = " " + COMPLEMENTO;
+		String _bairro = BAIRRO.isEmpty()? "" : BAIRRO + " - ";
+
+		if(MUNICIPIO.isEmpty()) {
+			System.out.printf("O campo município do endereço é obrigatório");
+			return;
+		}
+
+		String _municipio = MUNICIPIO + " - ";
+
+		if(ESTADO.isEmpty()) {
+			System.out.printf("O campo estado do endereço é obrigatório");
+			return;
+		}
+
+		String _cep = CEP.isEmpty()? "" : "CEP:" + CEP;
+		String _telefone = TELEFONE.isEmpty()? "" : "Tel " + TELEFONE;
+		_telefone = !_cep.isEmpty() && !_telefone.isEmpty()? " " + _telefone : _telefone;
+		
+		if (CNPJ.isEmpty()) {
+			System.out.printf("O campo CNPJ da loja é obrigatório");
+			return;
+		}
+
+		String _cnpj = "CNPJ: " + CNPJ;
+		
+		if (INSCRICAO_ESTADUAL.isEmpty()) {
+			System.out.printf("O campo inscrição estadual da loja é obrigatório");
+			return;
+		}
+
+		String _incricao_estadual = "IE: " + INSCRICAO_ESTADUAL;
+
+		System.out.println((NOME_LOJA + "\n" +
+			   _logradouro + _numero + _complemento + "\n" +
+			   _bairro + _municipio + ESTADO + "\n" +
+			   _cep + _telefone + "\n" +
+			   OBSERVACAO + "\n" +
+			   _cnpj + "\n" +
+			   _incricao_estadual).replace("\n", System.lineSeparator()));
+			   
+	}
+
+	
 	public static void main(final String[] args) {
-		//Implemente aqui
+		imprimir_dados();
 	}
 
 }
